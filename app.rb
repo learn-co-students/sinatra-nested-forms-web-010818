ff7fbfe47ea9aceea18bdc7c35663dfafe6f3614
feihafferkamp <fei.hafferkamp@gmail.com>
2018-01-31 16:34:37 -0500

    Done.

--- app.rb
+++ app.rb
@@ -3,7 +3,27 @@ require './environment'
 module FormsLab
   class App < Sinatra::Base
 
-    # code other routes/actions here
+    get '/' do
+      erb :root
+    end
+
+    get '/new' do
+      erb :'pirates/new'
+    end
+
+    post '/pirates' do
+      # params[:pirate] =>  {"name"=>"fei", "weight"=>"123", "height"=>"456"}
+
+      @new_pirate = Pirate.new(params[:pirate])
+
+      # params[:pirate][:ships] => [{"name"=>"fluffy", "type"=>"small", "booty"=>"fur"}, {"name"=>"hunky", "type"=>"metal", "booty"=>"peanuts"}]
+
+      params[:pirate][:ships].each do |ship_info|
+        Ship.new(ship_info)
+      end
+      @ships = Ship.all
+      erb :'pirates/show'
+    end
 
   end
 end
