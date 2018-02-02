3bb26695772457b01800f88bb634cbaba6b96033
krdiamond <github email address>
2018-02-02 10:33:17 -0500

    solved lab

--- app.rb
+++ app.rb
@@ -3,7 +3,31 @@ require './environment'
 module FormsLab
   class App < Sinatra::Base
 
-    # code other routes/actions here
+    get '/new' do
+      erb :'/pirates/new'
+    end
+
+    post '/pirates' do
+      @pirate = Pirate.new(params[:pirate])
+      erb  :'/pirates/show'
+    end
+
+
+    get '/' do      #this is the url
+      erb :root     #this is the file to look at
+    end
+
 
   end
 end
+
+
+
+
+# {"pirate"=>
+#   {"name"=>"Ian",
+#    "weight"=>"165",
+#    "height"=>"95",
+#    "ships"=>
+#     [{"name"=>"Titanic", "type"=>"Steam Liner", "booty"=>"Silver"},
+#      {"name"=>"Carpathia", "type"=>"Rescue Ship", "booty"=>"Inner Tubes"}]}}
--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,16 @@
 class Pirate
+  attr_accessor :name, :weight, :height
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+
 end
